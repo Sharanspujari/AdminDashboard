bf9cbc3b55cd7844e7a75d13981449daa9565a69
Sharan <pujarisp142@gmail.com>
2023-04-19 23:55:01 +0530

    head-bar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import {GrNotification,GrLanguage,FiSettings} from 'react-icons/all'
 function Header() {
   return (
-    <div>Header</div>
+   
+    <div className='header-div'>
+        <div className='container'>
+            <div className='left'>
+            <span className='logo'>Dashboard</span>
+            </div>
+            <div className='right'>
+                <div className='icons'>
+                <GrNotification/>
+                <span className='total-notifications'>4</span>
+                </div>
+                <div className='icons'>
+                <GrLanguage/>
+                <span className='total-notifications'>6</span>
+                </div>
+                
+                <div className='icons'>
+                <FiSettings/>
+               
+                </div>
+            </div>
+           
+          
+        </div>
+    </div>
+
   )
 }
 
