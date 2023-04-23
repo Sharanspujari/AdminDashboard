a1282f0ef019f870299ac980cbbdc28131f8db7d
Sharan <pujarisp142@gmail.com>
2023-04-23 13:18:36 +0530

    Added new user list

--- src/Components/Menu/Home.jsx
+++ src/Components/Menu/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Info from '../featureInfo/Info'
 import Chart from '../Charts/Chart'
 import { UserAnalyticsData } from '../../JsonData'
+import SmallWidges from '../UserSmallWidges/SmallWidges'
+import LargeWidges from '../UserLargeWidges/LargeWidges'
 function Home() {
   return (
     <div className='home-body'>
         <Info/>
         <Chart data={UserAnalyticsData} title="User Analytics" grid dataKey="Active user" name="name"/>
-       <div className='user-widges'>hello</div>
+       <div className='user-widges'>
+        <SmallWidges/>
+        <LargeWidges/>
+       </div>
     </div>
   )
 }
--- src/Components/UserSmallWidges/SmallWidges.jsx
+++ src/Components/UserSmallWidges/SmallWidges.jsx
@@ -1,8 +1,85 @@
 import React from 'react'
-
+import {MdVisibility} from 'react-icons/all'
 function SmallWidges() {
   return (
-    <div className='small-widges'>SmallWidges</div>
+    <div className='small-widges'>
+        <span className='member-heading'>New Members</span>
+       
+        <ul className='newuser-ul'>
+       
+        <li className='newuser-li'>
+      <img className='user-img' src='https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVuc3xlbnwwfHwwfHw%3D&w=1000&q=80' alt=' image not found'/>
+      
+       <div className='details-container'>
+   <span className='user-name'>Suprith</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+        Display
+       </button>
+       </li>
+
+       <li className='newuser-li'>
+       <img className='user-img' src='https://t3.ftcdn.net/jpg/02/36/48/86/360_F_236488644_opXVvD367vGJTM2I7xTlsHB58DVbmtxR.jpg' alt=' image not found'/>
+       <div className='details-container'>
+   <span className='user-name'>Abhishek</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+       Display
+       </button>
+       </li>
+
+       <li className='newuser-li'>
+       <img className='user-img' src='https://www.shutterstock.com/image-photo/web-camera-head-shot-computer-260nw-1863658792.jpg' alt=' image not found'/>
+       <div className='details-container'>
+   <span className='user-name'>Anjali</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+       Display
+       </button>
+       </li>
+
+       <li className='newuser-li'>
+       <img className='user-img' src='https://i0.wp.com/www.kahanihindi.com/wp-content/uploads/2021/06/Girls-profile-picture-8.jpg?resize=450%2C400&ssl=1' alt=' image not found'/>
+       <div className='details-container'>
+   <span className='user-name'>Ashika</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+       Display
+       </button>
+       </li>
+       <li className='newuser-li'>
+       <img className='user-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkQUg_P0cc8EgSCk-SdT8Yvcm6E5SbdfuhzA&usqp=CAU' alt=' image not found'/>
+       <div className='details-container'>
+   <span className='user-name'>Pallavi</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+       Display
+       </button>
+       </li>
+       <li className='newuser-li'>
+       <img className='user-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTalTnxL604AYAhRBLY8ALueAe3Z5yiauJxHA&usqp=CAU' alt=' image not found'/>
+       <div className='details-container'>
+   <span className='user-name'>Arjun</span>
+   <span className='user-job'>Software Engineer</span>
+       </div>
+       <button className='display-btn'>
+       <MdVisibility className='display-icon'/>
+       Display
+       </button>
+       </li>
+       </ul>
+    </div>
   )
 }
 
